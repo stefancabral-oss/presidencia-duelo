4273ad1f9f7753183b4abe4621ce34afb0025d11
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:12:51 -0300

    feat: ativar novo layout estrutural do Duelo no PWA

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,16 @@
 import { initGame } from "../../front/src/game.js";
 import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
 import { installFrontendEnhancements } from "../../front/src/frontend-enhancements.js";
+import { installDuelV2Layout } from "../../front/src/duel-v2-install.js";
 import "../../front/src/styles.css";
 import "../../front/src/malaquita.css";
 import "../../front/src/malaquita-shell.css";
+import "../../front/src/duel-v2.css";
 import { registerServiceWorker } from "./register-sw.js";
 
 installTournamentRestartGuard();
 registerServiceWorker();
-// Temporary compatibility mode: keep aggregate API voting available while
-// avoiding a hard dependency on player-sync endpoints during rollout.
-initGame().then(() => installFrontendEnhancements());
+initGame().then(() => {
+  installDuelV2Layout();
+  installFrontendEnhancements();
+});

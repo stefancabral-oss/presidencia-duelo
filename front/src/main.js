e55a63d6f1b5f89749045845c309215c091696bd
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:28:24 -0300

    feat(audio): ligar sound engine ao front

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,7 @@ import { initGame } from "./game.js";
 import { installTournamentRestartGuard } from "./restart-confirm.js";
 import { installFrontendEnhancements } from "./frontend-enhancements.js";
 import { installDuelV2Layout } from "./duel-v2-install.js";
+import { installInteractionSound } from "./interaction-sound.js";
 import "./styles.css";
 import "./malaquita.css";
 import "./malaquita-shell.css";
@@ -11,4 +12,5 @@ installTournamentRestartGuard();
 initGame().then(() => {
   installDuelV2Layout();
   installFrontendEnhancements();
+  installInteractionSound();
 });

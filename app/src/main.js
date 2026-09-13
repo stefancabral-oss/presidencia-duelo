01df9fee864de930bb600a1ddc1175c88402c24e
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 14:09:53 -0300

    feat: aplicar shell UX Malaquita no PWA

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,13 @@
 import { initGame } from "../../front/src/game.js";
 import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
+import { installFrontendEnhancements } from "../../front/src/frontend-enhancements.js";
 import "../../front/src/styles.css";
+import "../../front/src/malaquita.css";
+import "../../front/src/malaquita-shell.css";
 import { registerServiceWorker } from "./register-sw.js";
 
 installTournamentRestartGuard();
 registerServiceWorker();
 // Temporary compatibility mode: keep aggregate API voting available while
 // avoiding a hard dependency on player-sync endpoints during rollout.
-initGame();
+initGame().then(() => installFrontendEnhancements());

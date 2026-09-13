8992e68ac9a57ebf17f8c654de6228ff3cbe40fb
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:35:18 -0300

    feat(chromas): ligar entrada da galeria ao PWA

--- app/src/main.js
+++ app/src/main.js
@@ -2,10 +2,12 @@ import { initGame } from "../../front/src/game.js";
 import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
 import { installFrontendEnhancements } from "../../front/src/frontend-enhancements.js";
 import { installDuelV2Layout } from "../../front/src/duel-v2-install.js";
+import { installChromaEntry } from "../../front/src/chroma-entry.js";
 import "../../front/src/styles.css";
 import "../../front/src/malaquita.css";
 import "../../front/src/malaquita-shell.css";
 import "../../front/src/duel-v2.css";
+import "../../front/src/chroma-entry.css";
 import { registerServiceWorker } from "./register-sw.js";
 
 installTournamentRestartGuard();
@@ -13,4 +15,5 @@ registerServiceWorker();
 initGame().then(() => {
   installDuelV2Layout();
   installFrontendEnhancements();
+  installChromaEntry();
 });

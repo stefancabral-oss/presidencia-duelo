3c735113c90b946d0ee47021f9eeffee00a2f102
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 10:29:33 -0300

    fix: ativar confirmação não bloqueante no PWA

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,8 @@
 import { initGame } from "../../front/src/game.js";
+import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
 import "../../front/src/styles.css";
 import { registerServiceWorker } from "./register-sw.js";
 
+installTournamentRestartGuard();
 registerServiceWorker();
 initGame({ requireApi: true });

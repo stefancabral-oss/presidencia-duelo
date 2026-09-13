0b23905c30102042e98fe25b6dcdc1b3b16055ad
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 17:03:06 -0300

    feat(tournament): ativar torneio v2 no pwa

--- app/src/main.js
+++ app/src/main.js
@@ -2,15 +2,18 @@ import { initGame } from "../../front/src/game.js";
 import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
 import { installFrontendEnhancements } from "../../front/src/frontend-enhancements.js";
 import { installDuelV2Layout } from "../../front/src/duel-v2-install.js";
+import { installTournamentV2 } from "../../front/src/tournament-v2-install.js";
 import "../../front/src/styles.css";
 import "../../front/src/malaquita.css";
 import "../../front/src/malaquita-shell.css";
 import "../../front/src/duel-v2.css";
+import "../../front/src/tournament-v2.css";
 import { registerServiceWorker } from "./register-sw.js";
 
 installTournamentRestartGuard();
 registerServiceWorker();
 initGame().then(() => {
   installDuelV2Layout();
+  installTournamentV2();
   installFrontendEnhancements();
 });

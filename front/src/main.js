41a9bbb825d003315183f08a0b3d0b7bc642be01
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 17:01:44 -0300

    feat(rank): ativar ranking v2

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,16 @@ import { initGame } from "./game.js";
 import { installTournamentRestartGuard } from "./restart-confirm.js";
 import { installFrontendEnhancements } from "./frontend-enhancements.js";
 import { installDuelV2Layout } from "./duel-v2-install.js";
+import { installRankingV2 } from "./ranking-v2-install.js";
 import "./styles.css";
 import "./malaquita.css";
 import "./malaquita-shell.css";
 import "./duel-v2.css";
+import "./ranking-v2.css";
 
 installTournamentRestartGuard();
 initGame().then(() => {
   installDuelV2Layout();
+  installRankingV2();
   installFrontendEnhancements();
 });

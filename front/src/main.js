ae6427c2e9c361531ef643a6d22346dd8a033925
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 17:02:53 -0300

    feat(tournament): ativar torneio v2

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,16 @@ import { initGame } from "./game.js";
 import { installTournamentRestartGuard } from "./restart-confirm.js";
 import { installFrontendEnhancements } from "./frontend-enhancements.js";
 import { installDuelV2Layout } from "./duel-v2-install.js";
+import { installTournamentV2 } from "./tournament-v2-install.js";
 import "./styles.css";
 import "./malaquita.css";
 import "./malaquita-shell.css";
 import "./duel-v2.css";
+import "./tournament-v2.css";
 
 installTournamentRestartGuard();
 initGame().then(() => {
   installDuelV2Layout();
+  installTournamentV2();
   installFrontendEnhancements();
 });

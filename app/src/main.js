4e2acaf6a34fadde3af6d41ae89c0605b31c27f0
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:59:26 -0300

    style(home): carregar home v2 no pwa

--- app/src/main.js
+++ app/src/main.js
@@ -6,6 +6,7 @@ import "../../front/src/styles.css";
 import "../../front/src/malaquita.css";
 import "../../front/src/malaquita-shell.css";
 import "../../front/src/duel-v2.css";
+import "../../front/src/home-v2.css";
 import { registerServiceWorker } from "./register-sw.js";
 
 installTournamentRestartGuard();

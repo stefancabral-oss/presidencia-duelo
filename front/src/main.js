18db18b718a4ca96d9fbaed76154ad0a1707e151
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:35:05 -0300

    feat(chromas): ligar entrada da galeria ao front

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,16 @@ import { initGame } from "./game.js";
 import { installTournamentRestartGuard } from "./restart-confirm.js";
 import { installFrontendEnhancements } from "./frontend-enhancements.js";
 import { installDuelV2Layout } from "./duel-v2-install.js";
+import { installChromaEntry } from "./chroma-entry.js";
 import "./styles.css";
 import "./malaquita.css";
 import "./malaquita-shell.css";
 import "./duel-v2.css";
+import "./chroma-entry.css";
 
 installTournamentRestartGuard();
 initGame().then(() => {
   installDuelV2Layout();
   installFrontendEnhancements();
+  installChromaEntry();
 });

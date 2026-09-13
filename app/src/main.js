0c438dca07f29e5adfa545a2c4f0714e487a2209
stefancabral-oss <stefancabral@icloud.com>
2026-09-13 16:28:43 -0300

    feat(audio): ligar sound engine ao PWA

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import { initGame } from "../../front/src/game.js";
 import { installTournamentRestartGuard } from "../../front/src/restart-confirm.js";
 import { installFrontendEnhancements } from "../../front/src/frontend-enhancements.js";
 import { installDuelV2Layout } from "../../front/src/duel-v2-install.js";
+import { installInteractionSound } from "../../front/src/interaction-sound.js";
 import "../../front/src/styles.css";
 import "../../front/src/malaquita.css";
 import "../../front/src/malaquita-shell.css";
@@ -13,4 +14,5 @@ registerServiceWorker();
 initGame().then(() => {
   installDuelV2Layout();
   installFrontendEnhancements();
+  installInteractionSound();
 });
